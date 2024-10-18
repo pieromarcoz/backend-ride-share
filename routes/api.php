db2c41eb1a7917d355d5d853c2a8e9432f8dc8a8
pieromarcoz <75951868+pieromarcoz@users.noreply.github.com>
2024-10-18 17:52:01 -0500

    driver controller and routes api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\DriverController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,6 +9,8 @@ Route::post('/login', [LoginController::class, 'submit']);
 Route::post('/login/verify', [LoginController::class, 'verify']);
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('/driver', [DriverController::class, 'show']);
+    Route::post('/driver', [DriverController::class, 'update']);
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
